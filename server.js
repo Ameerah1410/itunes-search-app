0b3e0da9cbea60cfad504fecbc26a882729e3df1
Ameerah1410 <146541029+Ameerah1410@users.noreply.github.com>
2023-10-30 16:44:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000; // Set the port for the server to run on, defaulting to 5000 if no environment variable is set
 
-//if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
-//}
+// Serve static files and handle unspecified routes
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 // Handle GET request to the /search endpoint
 app.get("/search", async (req, res) => {
